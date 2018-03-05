1b84617cc81e73f3dde63ebad65d56e9e8677b65
vagrant <vagrant@homestead>
2018-03-05 08:39:35 +0000

    权限控制：非用户，非当前登录用户无法对当前用户编辑个人资料

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -28,11 +28,13 @@
                 @guest
                   <li><a href="{{ route('login') }}">登录</a></li>
                   <li><a href="{{ route('register') }}">注册</a></li>
-                @else
+                @endguest
+                @auth
+
                   <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                                <img src="{{ $user->avatar }}" width="30px" height="30px">
+                                <img src="{{ Auth::user()->avatar }}" width="30px" height="30px">
                             </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
@@ -58,7 +60,7 @@
                             </li>
                         </ul>
                     </li>
-                @endguest
+                @endauth
             </ul>
         </div>
     </div>
