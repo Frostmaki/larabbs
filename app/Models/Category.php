bcbc7e99ff9be1e7b961809497a4a77c2860d30b
vagrant <vagrant@homestead>
2018-03-09 03:35:02 +0000

    创建分类，并使用数据迁移保证不变

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    //.
+    protected $fillable = [
+        'name', 'description', ,
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+}
