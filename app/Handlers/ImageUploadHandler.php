0b7ccb3ff5bf09091a5efbacb6876fdd1b72606f
vagrant <vagrant@homestead>
2018-03-18 06:51:35 +0000

    设置默认头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -19,6 +19,8 @@ class ImageUploadHandler
         // 值如：/home/vagrant/Code/larabbs/public/uploads/images/avatars/201709/21/
         $upload_path = public_path() . '/' . $folder_name;
 
+        //return public_path();
+
         // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
         $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
 
